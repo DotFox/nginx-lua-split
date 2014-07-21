0061c9fae4e2b0c5ee2ce4cbb14cfa990f07ad7e
DeLaGuardo <delaguardo@gmail.com>
2014-07-22 02:27:29 +0700

    Initial commit

---
+++ build.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+
+LUAJIT_LIB="$LUAJIT/lib"
+LUAJIT_INC="$LUAJIT/include"
+
+ROOT=$PWD
+
+if [ ! -d "./build" ]; then
+  mkdir ./build
+fi
+# Cleanup before configure
+echo "Cleanup"
+for f in `find . -name \*.a`; do
+  rm $f
+done
+for f in `find . -name \*.o`; do
+  rm $f
+done
+rm -rf ./build/*
+echo "Clean up finished"
+
+# Grab nginx, nginx devel kit and nginx lua module
+echo "Saving sources"
+wget -q -O ./build/ngx_devel_kit.tar.gz https://github.com/simpl/ngx_devel_kit/archive/v0.2.19.tar.gz
+wget -q -O ./build/nginx.tar.gz http://nginx.org/download/nginx-1.5.12.tar.gz
+wget -q -O ./build/ngx_lua.tar.gz https://github.com/openresty/lua-nginx-module/archive/v0.9.10.tar.gz
+
+for f in `find ./build -name \*.tar.gz`; do
+  tar -zxf $f -C ./build
+  rm $f
+done
+
+echo "Complete saving sources"
+
+# Build all neccesery lua files into package
+LD_OPT="-L/usr/local/lib"
+mkdir ./build/libs
+for f in `find . -name \*.lua | grep -v build`; do
+  LIB_NAME=`basename $f .lua`.o
+  luajit -b $f ./build/libs/$LIB_NAME
+  LIB_PATH=$ROOT/build/libs/$LIB_NAME
+  LD_OPT="$LD_OPT $LIB_PATH"
+done
+
+cd ./build/nginx-1.5.12
+NGX_DEVEL_KIT=$PWD/../ngx_devel_kit-0.2.19
+LUA_NGINX_MODULE=$PWD/../lua-nginx-module-0.9.10
+CC_OPT="-I/usr/local/include -Wno-deprecated-declarations"
+
+if [ ! -d $LUAJIT_LIB ] || [ ! -d $LUAJIT_INC ]; then
+  echo "Not configure LUAJIT_LIB and LUAJIT_INC."
+  exit 1
+fi
+
+./configure \
+  --prefix=$PREFIX\
+  --add-module=$NGX_DEVEL_KIT\
+  --add-module=$LUA_NGINX_MODULE\
+  --add-module=`passenger-config --nginx-addon-dir`\
+  --with-cc-opt="$CC_OPT"\
+  --with-ld-opt="$LD_OPT"\
+  --with-pcre\
+  --with-ipv6\
+  --with-pcre-jit\
+  --with-http_ssl_module\
+  --with-http_gzip_static_module\
+  --with-http_stub_status_module
+
+make -j2
+make install
---
+++ split.lua
@@ -0,0 +1,213 @@
+local socket = require("socket")
+local redis = require("redis")
+
+math.randomseed(socket.gettime()*10000)
+
+-- Gap for cookie expire time, 1 week
+local cookie_gap = 7*24*60*60
+
+local simpleFormForEditExperiment = [[
+  <body>
+  <script>
+  var submit = function() {
+    var xmlHTTP = null;
+    var http = location.protocol;
+    var slashes = http.concat("//");
+    var host = slashes.concat(window.location.hostname);
+    var encode_params = function(data) {
+       var ret = [];
+       for (var d in data)
+          ret.push(encodeURIComponent(d) + "=" + encodeURIComponent(data[d]));
+       return ret.join("&");
+    }
+    var get_params = function() {
+      var params = {
+        name:       document.getElementById("name").value,
+        a:          document.getElementById("a").value,
+        b:          document.getElementById("b").value,
+        use_aff:    document.getElementById("use_aff").value,
+        stop_after: document.getElementById("stop_after").value
+      }
+      return params
+    }
+    var theUrl = host.concat("/api/split/?").concat(encode_params(get_params()));
+    xmlHttp = new XMLHttpRequest();
+    xmlHttp.open( "GET", theUrl, false );
+    xmlHttp.send( null );
+    return xmlHttp.responseText;
+  }
+  </script>
+  <div>
+    <h1>Configure A/B Experiment</h1>
+    <input id="name" type="text" placeholder="Experiment Name">
+    <input id="a" type="text" placeholder="A quotum">
+    <input id="b" type="text" placeholder="B quotum">
+    <input id="use_aff" type="text" placeholder="Is affiliate accepted">
+    <input id="stop_after" type="text" placeholder="Stop after">
+    <input type="submit" onClick="submit();">
+  </div>
+  </body>
+]]
+
+local function connectRedis()
+  local rds = redis:new()
+  rds:set_timeout(50)
+  local ok, err = rds:connect("127.0.0.1", 6379)
+  if not ok then
+    return nil
+  end
+  return rds
+end
+
+local function serializeExperiment(exp)
+  return exp.a .. "|" .. exp.b .. "|" .. exp.name .. "|" .. exp.use_aff .. "|" .. exp.stop_after
+end
+
+local function parseExperiment(exp)
+  local m, err = {}, nil
+  if exp then
+    if type(exp) == "string" then
+      local pattern = "(?<a>[0-9]+)[|](?<b>[0-9]+)[|](?<name>[a-z]+)[|](?<use_aff>[a-z]+)[|](?<stop_after>[0-9]+)"
+      m, err = ngx.re.match(exp, pattern)
+    elseif type(exp) == "table" then
+      stop_after = os.time() + tonumber(exp.arg_stop_after) * 60 * 60
+      m = {
+        a          = exp.arg_a,
+        b          = exp.arg_b,
+        name       = exp.arg_name,
+        use_aff    = exp.arg_use_aff,
+        stop_after = stop_after
+      }
+    end
+  end
+  return m
+end
+
+local function getExperiment(exp)
+  local experiment, err = {}, nil
+  if exp then
+    experiment = parseExperiment(exp)
+  else
+    local rds = connectRedis()
+    experiment, err = rds:get("experiment")
+    if type(experiment) == "string" then
+      experiment = parseExperiment(experiment)
+    elseif type(experiment) == "userdata" then
+      experiment = {}
+    end
+    rds:close()
+  end
+  return experiment, err
+end
+
+local function deleteExperiment(exp)
+  local rds = connectRedis()
+  local ok, err = rds:set("experiment", "")
+end
+
+local function saveExperiment(exp)
+  local message = "Experiment not saved."
+  local rds = connectRedis()
+  local str_experiment = serializeExperiment(exp)
+  local ok, err = rds:set("experiment", str_experiment)
+  if ok == "OK" then
+    message = "Experiment " .. exp.name .. " saved."
+    ok, err = rds:expire("experiment", exp.stop_after - os.time())
+    if ok == 1 then
+      message = message .. "\nExperiment will expired after " .. exp.stop_after - os.time() .. " seconds."
+    else
+      message = message .. "\nWarning! TTL for experiment not set."
+    end
+  end
+  rds:close()
+  return message
+end
+
+local function isValid(experiment)
+  local valid = true
+  local n = 0
+  for k, v in pairs(experiment) do
+    n = n + 1
+    if not v then
+      valid = false
+    end
+  end
+  if n == 0 then
+    valid = false
+  end
+  return valid
+end
+
+
+local function needAffiliateExperiment(marker, use_aff)
+  if marker and (string.match(marker, "^%d%d%d%d%d.-.") or string.match(marker, "^%d%d%d%d%d.-$")) then
+    if use_aff == "true" then
+      return true
+    elseif use_aff == "false" then
+      return false
+    end
+  end
+  return true
+end
+
+local function rollDice(a, b)
+  local x = math.random(0, 100)
+  if x > 100 - tonumber(a) then
+    return "a"
+  elseif x < tonumber(b) then
+    return "b"
+  end
+  return "default"
+end
+
+local function saveCookie(exp, test_rule)
+  ngx.header["Set-Cookie"] = {
+    "test_name=" .. exp.name .. "; path=/; Expires=" .. ngx.cookie_time(exp.stop_after + cookie_gap) .. ";",
+    "test_rule=" .. test_rule .. "; path=/; Expires=" .. ngx.cookie_time(exp.stop_after + cookie_gap) .. ";",
+    "test_stop=" .. exp.stop_after .. "; path=/; Expires=" .. ngx.cookie_time(exp.stop_after + cookie_gap) .. ";"
+  }
+end
+
+local function getTestKeyword(user_test_name, user_test_rule, user_test_stop, user_marker)
+  local test_rule = "default"
+  local current_exp, err = getExperiment()
+  if current_exp.name then
+    if needAffiliateExperiment(user_marker, current_exp.use_aff) then
+      if current_exp.name == user_test_name then
+        test_rule = user_test_rule
+        if current_exp.stop_after ~= user_test_stop then
+          saveCookie(current_exp, test_rule)
+        end
+      else
+        test_rule = rollDice(current_exp.a, current_exp.b)
+        saveCookie(current_exp, test_rule)
+      end
+    end
+  end
+  return test_rule
+end
+
+_M = {}
+
+function _M.saveExperiment()
+  local exp, err = getExperiment(ngx.var)
+  local message = "Experiment not configured."
+  if not err and isValid(exp) then
+    message = saveExperiment(exp)
+  end
+  return message
+end
+
+function _M.renderExperimentForm()
+  return simpleFormForEditExperiment
+end
+
+function _M.configureUser()
+  local test_rule = ngx.var.cookie_test_rule
+  local test_name = ngx.var.cookie_test_name
+  local test_stop = ngx.var.cookie_test_stop
+  local marker = ngx.var.arg_marker or ngx.var.cookie_marker
+  return getTestKeyword(test_name, test_rule, test_stop, marker)
+end
+
+return _M
