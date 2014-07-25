d44aea2d2d4a69c1d67494196dda9fafbf07818c
DeLaGuardo <delaguardo@gmail.com>
2014-07-25 14:44:08 +0700

    Add sudo support

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [ $1 == '--help' ]; then
+if [[ $1 == --help ]]; then
   echo "Build nginx with lua-nginx and injected A/B engine"
   echo ""
   echo "Run script with:"
@@ -13,37 +13,27 @@ fi
 
 NGINX_VERSION=1.5.12
 
-if [ "$NGINXV" == "1.5.12" || "$NGINXV" == "1.7.2" ]; then
+if [[ $NGINXV == 1.5.12 || $NGINXV == 1.7.2 ]]; then
   NGINX_VERSION=$NGINXV
-fi
-
-if [ ! $LUAJIT ]; then
-  echo "Luajit can not be installed in specified directory."
-  exit 1
 fi
 
 ROOT=$PWD
 
-if [ ! -d "./build" ]; then
-  mkdir ./build
-fi
 # Cleanup before configure
 echo "Cleanup"
-for f in `find . -name \*.a`; do
-  rm $f
-done
-for f in `find . -name \*.o`; do
-  rm $f
-done
-rm -rf ./build/*
+rm -rf $ROOT/build
 echo "Clean up finished"
+
+echo -n "Need sudo? Type yes or leave blanc. [ENTER]: "
+read NEED_SUDO
 
 # Grab nginx, nginx devel kit and nginx lua module
 echo "Saving sources"
-wget -q -O ./build/ngx_devel_kit.tar.gz https://github.com/simpl/ngx_devel_kit/archive/v0.2.19.tar.gz
-wget -q -O ./build/nginx.tar.gz http://nginx.org/download/nginx-$NGINX_VERSION.tar.gz
-wget -q -O ./build/ngx_lua.tar.gz https://github.com/openresty/lua-nginx-module/archive/v0.9.10.tar.gz
-git clone http://luajit.org/git/luajit-2.0.git ./build/luajit_src
+mkdir $ROOT/build
+wget -q -O $ROOT/build/ngx_devel_kit.tar.gz https://github.com/simpl/ngx_devel_kit/archive/v0.2.19.tar.gz
+wget -q -O $ROOT/build/nginx.tar.gz http://nginx.org/download/nginx-$NGINX_VERSION.tar.gz
+wget -q -O $ROOT/build/ngx_lua.tar.gz https://github.com/openresty/lua-nginx-module/archive/v0.9.10.tar.gz
+git clone http://luajit.org/git/luajit-2.0.git $ROOT/build/luajit_src
 
 for f in `find ./build -name \*.tar.gz`; do
   tar -zxf $f -C ./build
@@ -55,9 +45,14 @@ echo "Complete saving sources"
 echo "Build luajit"
 cd build/luajit_src
 git checkout v2.1
-make PREFIX=$LUAJIT
-make install PREFIX=$LUAJIT
-ln -sf $LUAJIT/bin/luajit-2.1.0-alpha $LUAJIT/bin/luajit
+make
+if [[ $NEED_SUDO != '' ]]; then
+  sudo make install PREFIX=$LUAJIT
+  sudo ln -sf $LUAJIT/bin/luajit-2.1.0-alpha $LUAJIT/bin/luajit
+else
+  make install PREFIX=$LUAJIT
+  ln -sf $LUAJIT/bin/luajit-2.1.0-alpha $LUAJIT/bin/luajit
+fi
 LUAJIT_BIN=$LUAJIT/bin/luajit
 LUAJIT_LIB=$LUAJIT/lib
 LUAJIT_INC=$LUAJIT/include/luajit-2.1
@@ -66,16 +61,15 @@ echo "luajit configured"
 
 # Build all neccesery lua files into package
 LD_OPT="-L/usr/local/lib"
-mkdir ./build/libs
-for f in `find . -name \*.lua | grep -v build`; do
+mkdir $ROOT/build/libs
+for f in `find $ROOT/libs -name \*.lua | grep -v build`; do
   LIB_NAME=`basename $f .lua`.o
-  $LUAJIT_BIN -b $f ./build/libs/$LIB_NAME
+  $LUAJIT_BIN -b $f $ROOT/build/libs/$LIB_NAME
   LIB_PATH=$ROOT/build/libs/$LIB_NAME
   LD_OPT="$LD_OPT $LIB_PATH"
 done
-LD_OPT="$LD_OPT -Wl,-rpath,$LUAJIT_LIB"
 
-cd ./build/nginx-$NGINX_VERSION
+cd $ROOT/build/nginx-$NGINX_VERSION
 NGX_DEVEL_KIT=$PWD/../ngx_devel_kit-0.2.19
 LUA_NGINX_MODULE=$PWD/../lua-nginx-module-0.9.10
 CC_OPT="-I/usr/local/include -Wno-deprecated-declarations"
@@ -98,4 +92,8 @@ LUAJIT_LIB=$LUAJIT_LIB LUAJIT_INC=$LUAJIT_INC ./configure \
   --with-http_stub_status_module
 
 make -j2
-make install
+if [[ $NEED_SUDO != '' ]]; then
+  sudo make install
+else
+  make install
+fi
